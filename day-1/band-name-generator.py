518488dce21f547c933c99eb803469cd7d633143
Pedro Sato <pedro.hiro@gmail.com>
2023-12-20 01:59:52 -0300

    day 1

---
+++ day-1/band-name-generator.py
@@ -0,0 +1,3 @@
+city = input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?")
+pet_name = input("What's your pet's name?")
+print("Your band name could be " + city + " " +  pet_name)
